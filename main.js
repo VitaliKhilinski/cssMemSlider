de140f77d839481ee7b591086f88dcdaae99d144
Vitali <v.khilinski@gmail.com>
2022-05-11 16:55:52 +0300

    feat: add basic page layout (Wed, 11 May 2022 16:55:39)

---
+++ main.js
@@ -0,0 +1,26 @@
+const imgContent = {
+  slide_1: {
+    img: "./assets/img/1",
+    description: "Гайд по Достоевскому",
+  },
+  slide_2: {
+    img: "./assets/img/2",
+    description: "Дворовой форсаж",
+  },
+  slide_3: {
+    img: "./assets/img/3",
+    description: "И ускоряешься, чтобы показать какая она быстрая",
+  },
+  slide_4: {
+    img: "./assets/img/4",
+    description: "Перукупы...",
+  },
+  slide_5: {
+    img: "./assets/img/5",
+    description: "Приоритеты...",
+  },
+  slide_6: {
+    img: "./assets/img/6",
+    description: "Рутина поглотила даже хоббита",
+  },
+};
